57a5443486c4d84ff9b78d41725ece1d690b206c
blocksombr31 <132086735+blocksombr31@users.noreply.github.com>
2025-10-28 16:58:42 -0500

    Created the general layout for the random user screen which contains a textfield and a list view

--- lib/random_user.dart
+++ lib/random_user.dart
@@ -1,10 +1,71 @@
 import 'package:flutter/material.dart';
 
-class RandomUser extends StatelessWidget {
+// simply set random user state to _randomuserstate
+class RandomUser extends StatefulWidget {
   const RandomUser({super.key});
 
   @override
+  State<RandomUser> createState() => _RandomUserState();
+}
+
+class _RandomUserState extends State<RandomUser> {
+  // create the controller for our text field
+  final TextEditingController _controller = TextEditingController();
+
+  // create the list that will display data we fetch
+  final List<String> _users = ['Fake user here!'];
+
+  // function for actually getting data from random user website using
+  // the value from our text field
+  void _getData(String value) {
+    // todo
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Text('Random user page!');
+    // set the contents of this page to contain a column
+    // with a textfield and listview
+    return Padding(
+      padding: EdgeInsetsGeometry.all(10.0),
+      child: Column(
+        children: [
+          // set the controller of our text field and call function to get data
+          TextField(
+            controller: _controller,
+            onSubmitted:(value) => _getData(value),
+          ),
+          // add some space between text field and list for easier reading
+          const SizedBox(height: 10.0),
+          // ensure we have data to display, if we don't inform the user of
+          // that, and if we do display it in a listview
+          Expanded(
+            child: _users.isEmpty 
+            ?
+            Center (child: 
+              Text (
+                textAlign: TextAlign.center,
+                'No data to display yet!',
+                ),
+            )
+            :
+            ListView.separated(
+              separatorBuilder: (_, __) => const Divider(), 
+              itemCount: _users.length,
+              itemBuilder: (context, i) {
+                return ListTile(
+                  title: Text(_users[i])
+                );
+              }, 
+            )
+          )
+        ],
+      ),
+    );
   }
 }
