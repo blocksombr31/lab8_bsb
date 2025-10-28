5ac3b8e75c5a506f481a5a06b3986b2e5c1b9adc
blocksombr31 <132086735+blocksombr31@users.noreply.github.com>
2025-10-28 15:49:13 -0500

    Created the outline for the app, simply containing two tabs.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:lab8_bsb/random_user.dart';
+import 'package:lab8_bsb/movies.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,9 +12,37 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('API Exploratorium'),
+          actions: [
+            IconButton(onPressed: () {}, icon: const Icon(Icons.add)),
+            IconButton(onPressed: () {}, icon: const Icon(Icons.settings)),
+          ],
+          bottom: const TabBar(
+            tabs: [
+              Tab(text: 'Random User'),
+              Tab(text: 'Movies'),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            RandomUser(),
+            Movies(),
+          ],
         ),
       ),
     );
